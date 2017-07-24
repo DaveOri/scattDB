deef03cce473b4e89c9331e21e803087c3c1f186
DaveOri <davide.ori87@gmail.com>
2017-07-24 12:50:33 +0200

    added a couple of functions to extract useful scattering quantities

--- scattering.py
+++ scattering.py
@@ -26,16 +26,17 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import pandas as pd
+import numpy as np
 
-class scattering(object):
+class Scattering(object):
     def __init__(self):
         print('A scattering object has been created')
-        self.sigbk = None
-        self.sigabs = None
-        self.sigext = None
-        self.sigsca = None
+        self.sig_bk = None
+        self.sig_abs = None
+        self.sig_ext = None
+        self.sig_sca = None
 
-class scattDDSCAT(scattering):
+class ScattDDSCAT(Scattering):
     def __init__(self,filename=None):
         if filename is not None:
             scattfile = open(filename,'r')
@@ -43,8 +44,9 @@ class scattDDSCAT(scattering):
             self.lines=lines
             self.Ndipoles = int(lines[5].split()[0])
             self.d = float(lines[7].split()[0])
-            self.aeff = float(lines[8].split()[1])
-            self.wl = float(lines[9].split()[1])
+            self.aeff = float(lines[8].split()[1])*1e-3 # convert to millimeters
+            self.wl = float(lines[9].split()[1])*1e-3 # convert to millimeters
+            self.k = 2*np.pi/self.wl
             qs = lines[31].split()
             self.Qe = float(qs[1])
             self.Qa = float(qs[2])
@@ -52,6 +54,55 @@ class scattDDSCAT(scattering):
             self.Qbk = float(qs[6])
             self.g = float(qs[4])
             self.phase = pd.read_csv(filename,sep='\s+',header=37)
+            self.sig_bk = self.eff2xsect(self.Qbk)
+            self.sig_abs = self.eff2xsect(self.Qa)
+            self.sig_ext = self.eff2xsect(self.Qe)
+            self.sig_sca = self.eff2xsect(self.Qs)
+            idx_bk = self.phase[(self.phase.theta == 180.0) & (self.phase.phi == 0.0)].index[0]
+            back = self.phase.iloc[idx_bk]
+
+            self.S = np.array([[back['S_11'],back['S_12']],[back['S_21'],back['S_22']]]) # TODO extend to the full 4x4
+            self.Z = self.S/self.k**2.
             
+            #back = self.phase.iloc[-1]
+            #print(0.5*(back['S_11']+back['S_12']+back['S_21']+back['S_22'])/self.k**2.,self.sig_bk)
+            #print(0.5*(back['S_11']-back['S_12']-back['S_21']+back['S_22'])/self.k**2.,self.sig_bk)
             
+
+    def eff2xsect(self,eff):
+        return eff*(np.pi*self.aeff**2.)            
             
+    def radar_xsect(self, h_pol=True):
+    """Radar cross section for the current setup.    
+
+    Args:
+        scatterer: a Scatterer instance.
+        h_pol: If True (default), use horizontal polarization.
+        If False, use vertical polarization.
+
+    Returns:
+        The radar cross section.
+    """        
+        Z = self.Z
+        if h_pol:
+            return 2*np.pi*(Z[0,0]-Z[0,1]-Z[1,0]+Z[1,1])
+        else:
+            return 2.np.pi*(Z[0,0]-Z[0,1]-Z[1,0]+Z[1,1])
+        
+    def ldr(h_pol=True):
+        """
+        Linear depolarizarion ratio (LDR) for the current setup.
+    
+        Args:
+            scatterer: a Scatterer instance.
+            h_pol: If True (default), return LDR_h.
+            If False, return LDR_v.
+    
+        Returns:
+           The LDR.
+        """
+        Z = self.Z
+        if h_pol:
+            return (Z[0,0]-Z[0,1]+Z[1,0]-Z[1,1])/(Z[0,0]-Z[0,1]-Z[1,0]+Z[1,1])
+        else:
+            return (Z[0,0]+Z[0,1]-Z[1,0]-Z[1,1])/(Z[0,0]+Z[0,1]+Z[1,0]+Z[1,1])
