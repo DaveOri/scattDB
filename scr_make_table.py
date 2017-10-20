c4c193cb3f53d807ed78566095923dc5505c0cff
DaveOri <davide.ori87@gmail.com>
2017-10-20 16:27:18 +0200

    generalized method to run through Ben's particles

--- scr_make_table.py
+++ scr_make_table.py
@@ -23,7 +23,7 @@ freqs = ['13.4','35.6','94']
 
 melt2perc = lambda x: int(int(x)*0.01)
 
-Zlab = {'13.4':'ZKu','35.6':'ZKa','94':'ZW'}
+Zlab = {'13.4':'Ku','35.6':'Ka','94':'W'}
 Llab = {'13.4':'LKu','35.6':'LKa','94':'LW'}
 
 c = 299792458000. # mm/s
@@ -36,18 +36,15 @@ coeffu = lamu**4./(0.95*np.pi**5.)
 coeffa = lama**4./(0.95*np.pi**5.)
 coeffW = lamW**4./(0.75*np.pi**5.)
 
-cols = ['Ku_Ka','Ka_W','LDRka','melt','Dm']
-
-
-datatot = pd.DataFrame(columns=cols)
+cols = ['Dmax','X','Ku','Ka','W','ldr']
+indexes = np.arange(10000)
 for melt_frac in melt_fracs:
-    data = pd.DataFrame(columns=cols)
-    i=0
-    dists = {}
+    data = pd.DataFrame(index=indexes, columns=cols)
     for freq in freqs:
         scatt_folders = sorted(glob(scattfolder+'*aggregate2*'+'_f'+melt_frac+'_*'+freq))
         dist = scattering.ScattDist()
-        
+        freqidx = Zlab[freq]
+        i=0
         for fld in scatt_folders:
             shpstr, aeffstr = fld.split('/')[-1].split('AEFF')
             shape_folders = glob(shapefolder+shpstr+'*')
@@ -55,33 +52,25 @@ for melt_frac in melt_fracs:
             shp = shape.shapeDDA(shapefile)
             
             avgfiles = sorted(glob(fld+'/*.avg'))
-            if 'to' in aeffstr:
-                pass
-#                for avg in avgfiles:
-#                    scatt = scattering.ScattDDSCAT(avg) 
-#                    scatt.D = shp.find_dmax(aeff=scatt.aeff)
-#                    scatt.mass = shp.find_mass(aeff=scatt.aeff)
-#                    scatt.melt = shp.get_melted_fraction()
-#                    dist.add_scatterer(scatt)
-                    
-            else:
-#                pass
-                scatt = scattering.ScattDDSCAT(avgfiles[0]) 
+            for avg in avgfiles:
+                scatt = scattering.ScattDDSCAT(avg) 
                 scatt.D = shp.find_dmax(aeff=scatt.aeff)
                 scatt.mass = shp.find_mass(aeff=scatt.aeff)
                 scatt.melt = shp.get_melted_fraction()
-                #scatt.melt = float(shpstr[-7:-1])/10000.
-                dist.add_scatterer(scatt)
-        dists[freq] = dist
-
-
+                data.iloc[i][Zlab[freq]] = scatt.sig_bk
+                data.iloc[i]['Dmax'] = scatt.D
+                if freq == '35.6':
+                    data.iloc[i]['ldr'] = scatt.ldr
+                i = i + 1
+    data.sort('Dmax',inplace=True)
+    data.dropna(how='all',inplace=True)
+    data.to_csv('tables/BJ_agg2_'+str(melt2perc(melt_frac))+'.csv')
 
 
 freqs = {'000':'C','001':'X','002':'Ku','003':'Ka','004':'W','005':'89','006':'157'}
 freqs = {'001':'X','002':'Ku','003':'Ka','004':'W'}
 scattfolder = '/work/DBs/0/'
 
-cols = ['Dmax','X','Ku','Ka','W','ldr']#,'XKa','KaW']
 subfolders = glob(scattfolder+'*')
 data00 = pd.DataFrame(index=[x[len(scattfolder):] for x in subfolders],columns=cols)
 for subfolder in subfolders:
@@ -98,7 +87,6 @@ for subfolder in subfolders:
             data00.loc[Dstr,'ldr'] = scatt.ldr
 
 scattfolder = '/work/DBs/10/'
-
 subfolders = glob(scattfolder+'*')
 data10 = pd.DataFrame(index=[x[len(scattfolder):] for x in subfolders],columns=cols)
 for subfolder in subfolders:
@@ -118,3 +106,5 @@ data00 = data00.astype(float)
 data10 = data10.astype(float)
 data00.sort(columns='Dmax',inplace=True)
 data10.sort(columns='Dmax',inplace=True)
+data00.to_csv('tables/DO_00.csv')
+data10.to_csv('tables/DO_10.csv')
