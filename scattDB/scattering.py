corrected mass computation, 3f analysis completed for gamma PSD, attempt to get 3f from moving gaussian

scattDB/scattering.py
@@ -136,7 +136,7 @@ class ScattADDA(Scatterer):
         self.wl = float(get_line(lines,'lambda:').split()[-1])*1e-3 # convert to millimeters
         self.dpl= float(get_line(lines,'Dipoles/lambda:').split()[-1])
         self.d = self.wl/self.dpl
-        self.mass = self.Ndipoles*self.d**3.0*0.917e-3
+        self.mass = 0.917e-3*self.Ndipoles*self.d**3.0
         self.k = 2*np.pi/self.wl
 
         self.Qe = float(CSlines[1].split()[-1])

scr_plot_sigma.py
@@ -33,6 +33,8 @@ melt = '00'
 
 dataDO = pd.read_csv(tablesfolder+author+'_'+melt+'.csv')
 dataDO.drop('Unnamed: 0',axis=1,inplace=True)
+meltDO = pd.read_csv(tablesfolder+author+'_'+'10'+'.csv')
+meltDO.drop('Unnamed: 0',axis=1,inplace=True)
 
 author = 'BJ'
 dataBJ2 = pd.read_csv(tablesfolder+author+'_agg2_0'+'.csv')
@@ -44,14 +46,18 @@ dataBJ3.drop('Unnamed: 0',axis=1,inplace=True)
 author = 'dataJL_A0.0'
 dataJL = pd.read_csv(tablesfolder+author+'.csv')
 
-#plt.figure()
-#ax = plt.gca()
-#ax.scatter(dataBJ2.Dmax,dataBJ2.Ku,label='BJ2')
-#ax.scatter(dataBJ3.Dmax,dataBJ3.Ku,label='BJ3')
-#ax.scatter(dataJL.Dmax, 1e6*dataJL.Ub, label='JL')
-#ax.scatter(dataDO.Dmax, dataDO.Ku, label='DO')
-#ax.legend()
-#ax.grid()
+plt.figure()
+ax = plt.gca()
+#ax.scatter(dataBJ2.Dmax,dataBJ2.mkg,label='BJ2')
+#ax.scatter(dataBJ3.Dmax,dataBJ3.mkg,label='BJ3')
+ax.scatter(dataJL.Dmax, dataJL.mkg, label='JL')
+ax.scatter(dataDO.Dmax, dataDO.mkg, label='DO')
+ax.plot(dataDO.Dmax,8.9e-5*dataDO.Dmax**2.1,label='brandes')
+ax.plot(dataDO.Dmax,0.02414e-3*dataDO.Dmax**1.9,label='BF95')
+ax.legend()
+ax.grid()
+ax.set_yscale('log')
+ax.set_xscale('log')
 
 def IntPsd(D,s,psd):
     conc = psd(D)
@@ -61,31 +67,39 @@ def IntPsd(D,s,psd):
 def myGammaPSD(D0,mu):
     return psd.GammaPSD(D0=D0,Nw=1.,mu=mu,D_max=22)
 
+def Gauss(mu,sig):
+    def F(D):
+        x = D-mu 
+        return np.exp(x*x/(2.0*sig*sig))/(sig*np.sqrt(2.0*np.pi))
+    return F
 
+D0s = np.linspace(0.1,20.,30)
+mus = [-1.,0.,1.,2.,3.]
+#mus = [0.1,0.3,0.5,0.8,1]
+marks=[',','+','h','v','.']
 
 def f3plot(data,title='title',color=None):
-    D0s = np.linspace(0.1,20.,20)
-    mus = [-1.,0.,1.,2.,3.]
-    marks=[',','+','h','v','.']
     plt.figure()
     ax = plt.gca()
     for mu,m in zip(mus,marks):
-        Zx, Za, Zw, XKa, KaW, LDR = ([] for i in range(6))
-        
+        Zx, Za, Zw, XKa, KaW, LDR, IWC = ([] for i in range(7))
         for D0 in D0s:
-#            conc  = psd.GammaPSD(D0=D0,Nw=1.,mu=mu,D_max=22)
             conc  = myGammaPSD(D0,mu)
-            zx = 10.*np.log10(coeffx*IntPsd(data.Dmax,data.X,conc))
-            zu = 10.*np.log10(coeffu*IntPsd(data.Dmax,data.Ku,conc))
+            #conc = Gauss(D0,mu)
+            iwc = IntPsd(data.Dmax,data.mkg,conc)
+            IWC.append(iwc)
+            zx = 10.*np.log10(coeffx*IntPsd(data.Dmax,data.X,conc)/iwc)
+            zu = 10.*np.log10(coeffu*IntPsd(data.Dmax,data.Ku,conc)/iwc)
+            
             if np.isnan(zx):
                 zx = zu
             Zx.append(zx)
-            Za.append(10.*np.log10(coeffa*IntPsd(data.Dmax,data.Ka,conc)))
-            Zw.append(10.*np.log10(coeffw*IntPsd(data.Dmax,data.W,conc)))
+            Za.append(10.*np.log10(coeffa*IntPsd(data.Dmax,data.Ka,conc)/iwc))
+            Zw.append(10.*np.log10(coeffw*IntPsd(data.Dmax,data.W,conc)/iwc))
             LDR.append( 10.*np.log10(IntPsd(data.Dmax,data.ldr,conc)))
         XKa = np.array(Zx)-np.array(Za)
         KaW = np.array(Za)-np.array(Zw)
-        cbarlabel = color
+
         if color is None:
             colorv = D0s
             vmin=0
@@ -96,23 +110,25 @@ def f3plot(data,title='title',color=None):
             vmin = -30
             vmax = -20
             cbarlabel = 'LDR$_Ka$'
-        elif color == 'Zx':
-            colorv = Zx
-            print(max(Zx))
-            vmin = -20
-            vmax = 25
-        elif color == 'Zka':
-            colorv = Za
-            print(max(Za))
-            vmin = -20
-            vmax = 5
-        elif color == 'Zw':
-            colorv = Zw
-            print(max(Zw))
-            vmin = -30
-            vmax = 0
+        else:
+            cbarlabel = color + '/iwc    [dB/(g/m$^3$)]'
+            if color == 'Zx':
+                colorv = Zx
+                print(max(Zx))
+                vmin = 20
+                vmax = 40
+            elif color == 'Zka':
+                colorv = Za
+                print(max(Za))
+                vmin = 10
+                vmax = 25
+            elif color == 'Zw':
+                colorv = Zw
+                print(max(Zw))
+                vmin = 0
+                vmax = 15
 
-        s = ax.scatter(KaW,XKa,c=colorv,marker=m,label='$\mu=$ '+str(mu),vmin=vmin,vmax=vmax)
+        s = ax.scatter(KaW,XKa,c=colorv,marker=m,label='$\mu=$ '+str(mu),vmin=vmin,vmax=vmax,cmap='jet')
     ax.legend()
     ax.grid()
     ax.set_xlabel('DWR$_{Ka,W}$')
@@ -123,12 +139,23 @@ def f3plot(data,title='title',color=None):
     colorbar.set_label(cbarlabel)
     ax.set_title(title)
 
-f3plot(dataDO,'Davide dry',color='Zw')
-f3plot(dataBJ2,'BJ2 dry',color='Zw')
-f3plot(dataBJ3,'BJ3 dry',color='Zw')
+f3plot(dataDO,'Davide dry')
+f3plot(dataDO,'Davide dry',color='Zx')
+f3plot(dataBJ2,'BJ2 dry',color='Zx')
+#f3plot(dataBJ3,'BJ3 dry',color='Zw')
 dataJL.columns = ['Dmax','model','ELWP','mkg','Dmax.1','Rgyr','ldr','riming', 'Xa','Xs', 'X', 'Xe', 'Ua', 'Us', 'Ku', 'Ue', 'Aa', 'As', 'Ka', 'Ae', 'Wa','Ws', 'W', 'We']
 dataJL.X  = dataJL.X*1.0e6
 dataJL.Ku = dataJL.Ku*1.0e6
 dataJL.Ka = dataJL.Ka*1.0e6
 dataJL.W  = dataJL.W*1.0e6
+f3plot(dataJL,'Jussi unrimed',color='Zx')
+f3plot(dataJL,'Jussi unrimed')
+
+f3plot(dataDO,'Davide dry',color='Zka')
+f3plot(dataJL,'Jussi unrimed',color='Zka')
+
+f3plot(dataDO,'Davide dry',color='Zw')
 f3plot(dataJL,'Jussi unrimed',color='Zw')
+
+f3plot(dataDO,'Davide dry',color='ldr')
+f3plot(meltDO,'Davide melt',color='ldr')