corrected computation of LDR for population of particles

scr_plot_sigma.py
@@ -68,6 +68,10 @@ dataBJ2_30 = pd.read_csv(tablesfolder+author+'_agg2_30'+'.csv')
 dataBJ2_30.drop('Unnamed: 0',axis=1,inplace=True)
 dataBJ2_40 = pd.read_csv(tablesfolder+author+'_agg2_40'+'.csv')
 dataBJ2_40.drop('Unnamed: 0',axis=1,inplace=True)
+dataBJ2_49 = pd.read_csv(tablesfolder+author+'_agg2_49'+'.csv')
+dataBJ2_49.drop('Unnamed: 0',axis=1,inplace=True)
+dataBJ2_70 = pd.read_csv(tablesfolder+author+'_agg2_70'+'.csv')
+dataBJ2_70.drop('Unnamed: 0',axis=1,inplace=True)
 
 dataBJ3 = pd.read_csv(tablesfolder+author+'_agg3_0'+'.csv')
 dataBJ3.drop('Unnamed: 0',axis=1,inplace=True)
@@ -146,9 +150,11 @@ def f3profile(data,title='title',what=1.0,color=None,ax=None):
                 zx = zu
             Zx.append(zx)
             MRR.append(mrr)
-            Za.append(10.*np.log10(coeffa*IntPsd(data.Dmax,data.Ka,conc)/iwc))
+            zalin = IntPsd(data.Dmax,data.Ka,conc)
+            ldrka = IntPsd(data.Dmax,data.xpolKa,conc)/zalin
+            Za.append(10.*np.log10(coeffa*zalin/iwc))
             Zw.append(10.*np.log10(coeffw*IntPsd(data.Dmax,data.W,conc)/iwc))
-            LDR.append(10.*np.log10(IntPsd(data.Dmax,data.ldr,conc)))
+            LDR.append(10.*np.log10(ldrka))
         XKa = np.array(Zx)-np.array(Za)
         KaW = np.array(Za)-np.array(Zw)
         if type(what) is float:
@@ -163,7 +169,7 @@ def f3profile(data,title='title',what=1.0,color=None,ax=None):
             ax.set_xlabel('DWR     [dBZ]')
         elif what=='LDR':
             ax.plot(LDR,D0s,c=color,linestyle='-.',label='$\mu$='+str(mu))
-            ax.set_xlim([-35,-5])
+            ax.set_xlim([-40,-15])
             ax.set_xlabel('LDR     [dB]')
         ax.invert_yaxis()
         ax.set_ylabel('D$_0$')
@@ -192,9 +198,11 @@ def f3plot(data,title='title',color=None,ax=None):
             if np.isnan(zx):
                 zx = zu
             Zx.append(zx)
-            Za.append(10.*np.log10(coeffa*IntPsd(data.Dmax,data.Ka,conc)/iwc))
+            zalin = IntPsd(data.Dmax,data.Ka,conc)
+            ldrka = IntPsd(data.Dmax,data.xpolKa,conc)/zalin
+            Za.append(10.*np.log10(coeffa*zalin/iwc))
             Zw.append(10.*np.log10(coeffw*IntPsd(data.Dmax,data.W,conc)/iwc))
-            LDR.append( 10.*np.log10(IntPsd(data.Dmax,data.ldr,conc)))
+            LDR.append(10.*np.log10(ldrka))
         XKa = np.array(Zx)-np.array(Za)
         KaW = np.array(Za)-np.array(Zw)
 
@@ -205,8 +213,8 @@ def f3plot(data,title='title',color=None,ax=None):
             cbarlabel = '$D_0$'
         elif color == 'ldr':
             colorv = LDR
-            vmin = -30
-            vmax = -20
+            vmin = -35
+            vmax = -15
             cbarlabel = 'LDR$_Ka$'
         else:
             cbarlabel = color + '/iwc    [dB/(g/m$^3$)]'
@@ -230,7 +238,7 @@ def f3plot(data,title='title',color=None,ax=None):
     ax.grid()
     ax.set_xlabel('DWR$_{Ka,W}$')
     ax.set_ylabel('DWR$_{X,Ka}$')
-    ax.set_xlim([-1,15])
+    ax.set_xlim([0,20])
     ax.set_ylim([0,22])
     if set_colorbar:
         colorbar = plt.colorbar(mappable=s,ax=ax)
@@ -240,22 +248,22 @@ def f3plot(data,title='title',color=None,ax=None):
     return ax
 
 f3plot(dataDO,'Davide dry')
-f3plot(dataDO,'Davide dry',color='Zx')
-f3plot(dataBJ2,'BJ2 dry',color='Zx')
+#f3plot(dataDO,'Davide dry',color='Zx')
+#f3plot(dataBJ2,'BJ2 dry',color='Zx')
 
 dataJL.columns = ['Dmax','model','ELWP','mkg','Dmax.1','Rgyr','ldr','riming', 'Xa','Xs', 'X', 'Xe', 'Ua', 'Us', 'Ku', 'Ue', 'Aa', 'As', 'Ka', 'Ae', 'Wa','Ws', 'W', 'We']
 dataJL.X  = dataJL.X*1.0e6
 dataJL.Ku = dataJL.Ku*1.0e6
 dataJL.Ka = dataJL.Ka*1.0e6
 dataJL.W  = dataJL.W*1.0e6
-f3plot(dataJL,'Jussi rimed A0.5',color='Zx')
-f3plot(dataJL,'Jussi rimed A0.5')
+#f3plot(dataJL,'Jussi rimed A0.5',color='Zx')
+#f3plot(dataJL,'Jussi rimed A0.5')
 
-f3plot(dataDO,'Davide dry',color='Zka')
-f3plot(dataJL,'Jussi rimed A0.5',color='Zka')
+#f3plot(dataDO,'Davide dry',color='Zka')
+#f3plot(dataJL,'Jussi rimed A0.5',color='Zka')
 
-f3plot(dataDO,'Davide dry',color='Zw')
-f3plot(dataJL,'Jussi rimed A0.5',color='Zw')
+#f3plot(dataDO,'Davide dry',color='Zw')
+#f3plot(dataJL,'Jussi rimed A0.5',color='Zw')
 
 ax = f3plot(dataDO,'Davide dry',color='ldr')
 f3plot(meltDO,'Davide 0 - 10 %',color='ldr',ax=ax)
@@ -264,18 +272,21 @@ ax.grid()
 ax = f3plot(dataBJ2,'BJ2 dry',color='ldr')
 f3plot(dataBJ2_10,'BJ2 10%',color='ldr',ax=ax)
 f3plot(dataBJ2_20,'BJ2 20%',color='ldr',ax=ax)
-f3plot(dataBJ2_30,'BJ2 0 - 30%',color='ldr',ax=ax)
+f3plot(dataBJ2_30,'BJ2 30%',color='ldr',ax=ax)
+f3plot(dataBJ2_40,'BJ2 40%',color='ldr',ax=ax)
+f3plot(dataBJ2_49,'BJ2 49%',color='ldr',ax=ax)
+f3plot(dataBJ2_70,'BJ2 0 - 70%',color='ldr',ax=ax)
 ax.grid()
 
 
-f3profile(dataJL,title='Jussi unrimed')
-f3profile(dataJL,title='Jussi unrimed 0.01 kg/m$^2$',what=0.01)
-f3profile(dataJL,title='Jussi unrimed',what='DWR')
+#f3profile(dataJL,title='Jussi unrimed')
+#f3profile(dataJL,title='Jussi unrimed 0.01 kg/m$^2$',what=0.01)
+#f3profile(dataJL,title='Jussi unrimed',what='DWR')
 
-f3profile(dataDO,title='Davide dry')
-f3profile(dataDO,title='Davide dry 0.01 kg/m$^2$',what=0.01)
-f3profile(dataDO,title='Davide dry',what='DWR')
+#f3profile(dataDO,title='Davide dry')
+#f3profile(dataDO,title='Davide dry 0.01 kg/m$^2$',what=0.01)
+#f3profile(dataDO,title='Davide dry',what='DWR')
 f3profile(dataDO,title='Davide dry',what='LDR')
-f3profile(dataRH,title='RH spherical')
-f3profile(dataRH,title='RH spherical',what='DWR')
-f3plot(dataRH,'RH spherical')
+#f3profile(dataRH,title='RH spherical')
+#f3profile(dataRH,title='RH spherical',what='DWR')
+#f3plot(dataRH,'RH spherical')